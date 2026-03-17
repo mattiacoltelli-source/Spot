d304ec0368ff6edb99aad0da69cc1e5bdba1c6fd
mattiacoltelli-source <mattia.coltelli@gmail.com>
2026-03-17 16:55:32 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,7 @@
-const CACHE_NAME = "photospot-v3-kumi";
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll([
-      "./",
-      "./index.html",
-      "./manifest.json",
-      "./icon-192.png"
-    ]))
-  );
+self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }))
-    )
-  );
+self.addEventListener("activate", () => {
   self.clients.claim();
-});
-
-self.addEventListener("fetch", e => {
-  if (e.request.method !== "GET") return;
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
 });
